9176b7b31961845d296ac7fc95499231ff5614ba
nitishranjan27 <nk47595@gmail.com>
2022-02-05 21:55:49 +0530

    Nitish Added UC1 Program

--- Address_Book_System/Program.cs
+++ Address_Book_System/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+namespace Address_Book_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program \n");
+
+            // UC1 : Ability to create a Contacts in Address Book with first and last names, address,
+            // city, state, zip, phone number and email...
+            AddressBook.Display();
+        }
+    }
+}
